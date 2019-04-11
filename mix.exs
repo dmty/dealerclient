c7bda6ce70303980006151c48bce7d7ebb19dbfc
Dmitry Yakovlev <dmtyak@gmail.com>
2019-04-11 13:52:43 +1200

    Added Absinthe for GraphQL

--- mix.exs
+++ mix.exs
@@ -36,7 +36,9 @@ defmodule DealerClient.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:absinthe_plug, "~> 1.4"},
+      {:poison, "~> 2.2.0"},
     ]
   end
 end
